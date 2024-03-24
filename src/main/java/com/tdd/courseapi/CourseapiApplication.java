a914e350f5d8e7776ccba03a324420d65c6acf33
wwwkang8 <wwwkang8@gmail.com>
2024-03-24 11:01:40 +0900

    first commit

---
+++ src/main/java/com/tdd/courseapi/CourseapiApplication.java
@@ -0,0 +1,13 @@
+package com.tdd.courseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseapiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CourseapiApplication.class, args);
+  }
+
+}
