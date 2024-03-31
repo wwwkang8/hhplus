6e4bb0efc895ce4c45d41239e8c1dffad9d4906c
wwwkang8 <wwwkang8@gmail.com>
2024-03-31 13:04:01 +0900

    initial commit

---
+++ src/test/java/com/tdd/concert/ConcertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdd.concert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcertApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
