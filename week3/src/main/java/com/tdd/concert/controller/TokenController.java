c904f96798eb56beddf4ee72197d9dac8fd9f738
wwwkang8 <wwwkang8@gmail.com>
2024-03-31 21:22:32 +0900

    추가:Token, Reservation 컨트롤러 어노테이션 추가

--- week3/src/main/java/com/tdd/concert/controller/TokenController.java
+++ week3/src/main/java/com/tdd/concert/controller/TokenController.java
@@ -1,7 +1,8 @@
 package com.tdd.concert.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class TokenController {
 }
